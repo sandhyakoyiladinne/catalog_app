ad526aa340e75743f6c6278895ca5633f31a2ed4
SandhyaReddy <sandhya23@navgurukul.org>
2024-10-08 12:18:10 +0530

    changes

--- src/pages/firebase.jsx
+++ src/pages/firebase.jsx
@@ -17,26 +17,3 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app); // Get the auth instance
 
 export { auth }; // Export the auth instance
-
-
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-
-// const firebaseConfig = {
-//   REACT_APP_FIREBASE_API_KEy = "AIzaSyCjgjuNRCN5X3J3cQpHjB8UKUgFe-j0jT4",
-//   REACT_APP_FIREBASE_AUTH_DOMAIN= "movie-catlog-app.firebaseapp.com",
-//   REACT_APP_FIREBASE_PROJECT_ID= "movie-catlog-app",
-//   REACT_APP_FIREBASE_STORAGE_BUCKET="movie-catlog-app.appspot.com",
-//   REACT_APP_FIREBASE_STORAGE_BUCKET= "266709371263",
-//   REACT_APP_FIREBASE_APP_ID= "1:266709371263:web:f6b44f62302a94d4a4324a",
-//   REACT_APP_FIREBASE_MEASUREMENT_ID= "G-P5P9CZTMWQ"
-// };
-
-// Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const auth = getAuth(app); // Get the auth instance
-
-// export { auth }; // Export the auth instance
-
-
-
